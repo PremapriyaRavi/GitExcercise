a05fef7457d8de20963d7358b1cfc810c875bce3
PremapriyaRavi <riyaaravi97@gmail.com>
2021-09-10 13:59:44 -0700

    Shobi Code

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test1");
 		System.out.println("test2");
+		System.out.println("test5");
+		System.out.println("test6");
+		System.out.println("test7");
 	}
 
 }
