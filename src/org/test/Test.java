3a2abe619e18ba63c5665d6f1af199cce7c01c34
PremapriyaRavi <riyaaravi97@gmail.com>
2021-09-13 13:41:34 -0700

    newly add

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -2,16 +2,9 @@ package org.test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("test1");
-		System.out.println("test2");
-<<<<<<< HEAD
-		System.out.println("test5");
-		System.out.println("test6");
-		System.out.println("test7");
-=======
-		System.out.println("test3");
-		System.out.println("test4");
->>>>>>> 0398b95c3558f4bbf4e3fce5ca14b751fbb06b6f
+		
+		System.out.println("test 10");
+		System.out.println("test 11");
+		
 	}
-
 }
