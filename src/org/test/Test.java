a5e06871ebccc9d12a23674bd708353393e6759a
PremapriyaRavi <71207806+PremapriyaRavi@users.noreply.github.com>
2021-09-13 01:17:15 -0700

    Update Test.java

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -3,6 +3,11 @@ package org.test;
 public class Test {
 	public static void main(String[] args) {
 		
+		System.out.println("test 1");
+		System.out.println("test 2");
+		System.out.println("test 5");
+		System.out.println("test 6");
+		System.out.println("test 7");
 		System.out.println("test 10");
 		System.out.println("test 11");
 		
